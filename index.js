365d53453d4448fb24b01ee7ee7638582f0119fb
Jahaziel <jahaziel.vaz@gmail.com>
2019-05-17 19:22:15 -0600

    Sample of redux without react

---
+++ index.js
@@ -0,0 +1,22 @@
+const { createStore } = require('redux');
+
+const initialState = {
+  age: 30
+}
+
+const reducer = (state = initialState, action) => {
+  const newState = {...state}
+
+  if(action.type === 'ADD'){
+    newState.age += 1;
+  }
+
+  return newState
+}
+
+
+const store = createStore(reducer)
+
+store.dispatch({type: 'ADD'})
+
+console.log(store.getState() )
